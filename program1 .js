0913ecd7ebb1ac8546cc19bef0ea4b9f3fd0f286
miki119955 <47865250+miki119955@users.noreply.github.com>
2019-02-21 21:26:45 +0200

    Create program1 .js

---
+++ program1 .js
@@ -0,0 +1,14 @@
+/. 
+var x = prompt ("enter first number" , 0)
+var y = prompt ("enter second number" , 0)
+
+if (x>y)
+  {
+  console.log (" x bigger")
+  }
+ else 
+ { 
+   console.log ( "y is bigger")
+   
+ }
+
